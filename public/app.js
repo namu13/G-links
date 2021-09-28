64a9dc2f525e516c5847b28216f6c7d9d96c9be1
chan-kim <chan.ckeem@gmail.com>
2021-09-28 23:41:44 +0900

    C 2021.9.28.11:41(PM)
    add button toggle system

--- public/app.js
+++ public/app.js
@@ -5,3 +5,8 @@ db.collection("teacher")
   .then((result) => {
     console.log(result);
   });
+
+function radio_chk(value) {
+  const btn = document.getElementById(value);
+  btn.classList.toggle('button_clicked');
+}
