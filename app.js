24387af7081a84a33419bb8d7878f708c6ee6a52
Namun7 <77545233+Namun7@users.noreply.github.com>
2021-10-06 17:41:29 +0900

    D 2021.10.6.5:40
    eliminate firebase

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// firebase.initializeApp(config);
-// const db = firebase.firestore();
-// db.collection("")
-//   .get()
-//   .then((result) => {
-//     console.log(result);
-//   });
-
 // function radio_chk(idName) {
 //   const btn = document.getElementById(idName);
 //   const radio_btn = document.getElementsByName(this.name);
