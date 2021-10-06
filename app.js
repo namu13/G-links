2675731e92ec90931ae921743c871cafb1fadb6a
chan-kim <chan.ckeem@gmail.com>
2021-10-06 20:52:24 +0900

    C 2021.10.6.8:51(PM)
    icon size up

--- app.js
+++ app.js
@@ -11,7 +11,6 @@
 // }
 
 const teachersLinksButton = document.getElementsByName('checkbox');
-console.dir(teachersLinksButton);
 teachersLinksButton[0].checked = true;
 
 
@@ -70,7 +69,7 @@ function renderBox(classification) {
       boxItemTitle.innerText = boxItemName;
       // .box__item__connect
       const boxItemConnect = document.createElement("i");
-      boxItemConnect.classList.add("fas", "fa-play-circle", "fa-4x");
+      boxItemConnect.classList.add("fas", "fa-play-circle");
       boxItem.appendChild(boxItemConnect);
       // .box__item__copy
       const boxItemCopy = document.createElement("i");
