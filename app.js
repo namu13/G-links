5412ba18eb2d4f1a00f7d827dfbb48b7fc14973f
chan-kim <chan.ckeem@gmail.com>
2021-10-16 00:10:48 +0900

    C 2021.10.16.00:05(AM)
    일주일...만에...코딩이다아악!!!!
    try if button clicked show different page but failed. I will try again the next day.

--- app.js
+++ app.js
@@ -96,7 +96,18 @@ function renderBox(classification) {
   }
 }
 
-renderBox("teacher");
+document.getElementsByTagName('body').addEventListener('load', renderBox("teacher"))
+function btnClick() {
+  const btn1 = document.getElementById('checkbox-1');
+  const btn2 = document.getElementById('checkbox-2');
+  if (btn1.checked == true) {
+    renderBox("teacher")
+    console.log('1')
+  } else if (btn2.checked == true) {
+    renderBox("class")
+    console.log('2')
+  }
+}
 // 글자가 한줄을 넘어갔을때 조치가 필요함 - 필수
 
 // 블럭이 페이지 이상으로 추가됬을 때 조치가 필요함
