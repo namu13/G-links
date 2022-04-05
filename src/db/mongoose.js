c03b7959cf6516b0e36cdea14bd3a088f0173a9a
namu <namun1303@gmail.com>
2022-04-06 00:01:29 +0900

    hide mongodb api key

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,26 +1,28 @@
 const mongoose = require("mongoose");
-// const localUri = require("../../apiKeys");
-const Link_db = require("../models/link");
 
 const uri = process.env.MONGODB_URI;
 
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("connected to database!");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-// Link_db.find({ name: "도령" }).then((data) => {
-//   console.log(data);
-// });
+if (!uri) {
+  const localUri = async () => {
+    const { default: localUri } = await import("../../apiKeys.js");
+    mongoose
+      .connect(localUri)
+      .then(() => {
+        console.log("connected to database!");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-// const link_db = new Link_db({
-//   name: "몰라",
-//   class: "혜화",
-//   title: "2021 M4 수학 학습지",
-//   url: "https://www.icloud.com/numbers/0WWnoQITF12vuVu0M6_ukPpSQ",
-// });
-
-// link_db.save();
+  localUri();
+} else {
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("connected to database!");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
